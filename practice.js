8056cd64af49ded2e31648be6dadeaf223e8f49c
Bolaji Daniels Ilori <you@example.com>
2021-08-04 17:46:35 +0200

    First commit of 3-card component

---
+++ practice.js
@@ -0,0 +1,30 @@
+let data = [
+    {
+        name : 'Jude',
+        sex : 'Male',
+        height : 175
+    },
+
+    {
+        name : 'John', 
+        sex : 'Male',
+        height : 183
+    },
+    {
+        name : 'Jane', 
+        sex : 'Female',
+        height : 168
+    }
+]
+
+function geek(data){
+    for (i in data){
+        return data.map(dat.name, dat.sex)
+    }
+}
+console.log(Number(firstDecimal + secondDecimal))
+
+let firstInteger = 4
+let secondInteger = 8
+
+console.log(firstInteger + secondInteger)
